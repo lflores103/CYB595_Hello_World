5c4f09d7696655d033c1d89509317744fc7e527c
lou90 <lou90@LAPTOP-6LIKPI16>
2022-03-22 20:06:20 -0700

    Commit new class

---
+++ SmapleProject/src/JUnitPackage/JUnitExample.java
@@ -0,0 +1,15 @@
+package JUnitPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+public class JUnitExample {
+	@Test
+	public void test() {
+		System.out.println("Hello World I Work");
+        String str1="This is the testcase in this class";
+        assertEquals("This is the testcase in this class", str1);
+    }
+}
